e5a7dd55b9bdcdd48ebbf6798d1873a45ac49965
Светлана <svetlanagreenfox@gmail.com>
2021-11-08 16:17:36 +0300

    Added tests

--- src/__tests__/addall.test.js
+++ src/__tests__/addall.test.js
@@ -31,5 +31,39 @@ test('Add Characters', () => {
   const team = new Team();
   team.addAll(Bowerman, Magician, Swordsman);
 
-  expect(team).toEqual(Team { members: Set { 'Bowerman', 'Magician', 'Swordsman' } });
+  expect(team.toArray()).toEqual(['Bowerman', 'Magician', 'Swordsman']);
+})
+
+test('Add Characters', () => {
+  const Bowerman = {
+    name: 'Bowerman',
+    type: 'Bowerman',
+    health: 100,
+    level: 1,
+    attack: 25,
+    defence: 25,
+  };
+
+  const Magician = {
+    name: 'Magician',
+    type: 'Magician',
+    health: 100,
+    level: 1,
+    attack: 10,
+    defence: 40,
+  };
+
+  const Swordsman = {
+    name: 'Swordsman',
+    type: 'Magician',
+    health: 100,
+    level: 1,
+    attack: 40,
+    defence: 10,
+  };
+
+  const team = new Team();
+  team.addAll(Bowerman, Magician, Swordsman);
+
+  expect(team.toArray()).toEqual(['Bowerman', 'Magician']);
 })
